2188eeb56481e7b8853f3858fd1d25d2ce944a83
amontalvof <amontalvof92@gmail.com>
2023-02-07 09:21:41 -0600

    feat: Relationships

--- src/relationships.js
+++ src/relationships.js
@@ -1,6 +1,9 @@
-const fetchWithoutToken = require('./src/fetch');
+const fetchWithoutToken = require('./fetch');
 
+// the product with id 1 has 3 reviews
 // const url = 'products/1?_embed=reviews';
+
+// the review with id 1 has a product
 const url = 'reviews/1?_expand=product';
 
 fetchWithoutToken(url)
